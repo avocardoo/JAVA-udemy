9b072e7159962c6b5c46d37c0910579119611296
Maulik Bhatnagar <maulikbhatnagar987@gmail.com>
2023-07-20 00:21:42 +0530

    second commit
    till boolean datatype

---
+++ src/Boolean.java
@@ -0,0 +1,18 @@
+public class Boolean {
+    public static void main(String[] args) {
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+
+        System.out.println(myTrueValue);
+        System.out.println(myFalseValue);
+
+        //Test cases
+        /*
+        isCustomerOverTwentyOne
+        isEligibleForDiscount
+        hasValidLicense
+        isMarried
+        hasChildren
+         */
+    }
+}
---
+++ src/Character.java
@@ -0,0 +1,13 @@
+public class Character {
+    public static void main(String[] args) {
+//        char myChar = 'd';
+//        char myUnicode = 68;
+//        System.out.println(myChar);
+//        System.out.println(myUnicode);
+
+        char mySimpleChar = '?';
+        char myUnicodeChar = '\u003F';
+        char myDecimalChar = 63;
+        System.out.println("My values are " + mySimpleChar + myUnicodeChar + myDecimalChar);
+    }
+}
