021957d5120ca44f7134e0b9b527065f7f04992a
Maulik Bhatnagar <maulikbhatnagar987@gmail.com>
2023-08-07 23:37:12 +0530

    fewMore

---
+++ KeywordsAndExpressions/src/HighScore.java
@@ -0,0 +1,35 @@
+public class HighScore {
+    public static void displayHighScorePosition(String playerName, int highScorePosition){
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        int position = 4;
+        if (playerScore >= 1000){
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100){
+            position = 3;
+        }
+        return position;
+    }
+
+    public static void main(String[] args) {
+        int highScorePosition = calculateHighScorePosition(999);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(499);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Percy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(99);
+        displayHighScorePosition("Gilbert", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(-1000);
+        displayHighScorePosition("James", highScorePosition);
+    }
+
+}
